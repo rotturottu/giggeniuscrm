a6822946de3cbba2885660c9b494d5b6d982d2ca
qgedelacruz-svg <qgedelacruz@tip.edu.ph>
2026-02-27 01:57:28 +0800

    Uploaded main directory files

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
+import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginUnusedImports from "eslint-plugin-unused-imports";
-import globals from "globals";
 
 export default [
   {
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import base44 from "@base44/vite-plugin";
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import base44 from "@base44/vite-plugin"
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
